2fd84979725cfc798bb448e879ddc0d29d1908c4
sneha_mankhair <Sneha.Mankhair@blueconchtech.com>
2023-04-20 17:27:43 +0530

    Header Component Updated

--- src/components/common/ssp-header.jsx
+++ src/components/common/ssp-header.jsx
@@ -1,13 +1,19 @@
-import Image from 'next/image';
+import Image from "next/image";
 
-import { AppBar, Box, Toolbar, Typography  } from '@mui/material';
+import { AppBar, Toolbar } from "@mui/material";
+import styles from "./header.module.css";
 
 export default function Header() {
   return (
     <AppBar position="static" sx={{ backgroundColor: "white" }}>
       <Toolbar>
-        {/* <img src="/logo.svg" alt="SIXT" style={{ height: 20 }} /> */}
-        <Image src="/logo.svg" alt="SIXT" width="50" height="20"  />
+        <div className={styles.custome_size } style={{ position: "relative" }}>
+          <Image
+            src="/logo.svg"
+            alt="SIXT"
+            layout="fill"
+          />
+        </div>
       </Toolbar>
     </AppBar>
   );
