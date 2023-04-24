311ad13b6f99251d32df271aac925f97bc917ebf
himani.harkawat <himani.harkawat@blueconchtech.com>
2023-04-24 16:34:20 +0530

    welcome screen responsiveness fix

--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -3,23 +3,36 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import Loader from "./common/ssp-loader";
+import Grid from "@mui/material/Grid";
 
 export default function Welcome() {
   return (
     <>
-      <img
-        src="/ssp-logo.svg"
-        width="120"
-        height="50"
-        alt="Paris"
-        style={{
-          display: "block",
-          marginLeft: "auto",
-          marginRight: "auto",
-          marginTop: "40vh",
-        }}
-      />
-      <Loader isprimary="true" style={{ marginTop: "2%" }} />
+      <Grid
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid item xs={2}>
+          {" "}
+          <img
+            src="/ssp-logo.svg"
+            width="120"
+            height="50"
+            alt="Paris"
+            style={{
+              display: "block",
+              marginLeft: "auto",
+              marginRight: "auto",
+              marginTop: "40vh",
+            }}
+          />
+        </Grid>
+        <Grid item xs={2}>
+          <Loader isprimary="true" style={{ marginBottom: "40vh" }} />
+        </Grid>
+      </Grid>
     </>
   );
 }
