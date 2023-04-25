0159e286c4719960a22b0a4564975d7aa3951a7f
himani.harkawat <himani.harkawat@blueconchtech.com>
2023-04-25 10:35:58 +0530

    Responsiveness issue fix

--- src/components/authenticate.jsx
+++ src/components/authenticate.jsx
@@ -1,11 +1,10 @@
 import Header from "./common/ssp-header";
-import Footer from "./common/ssp-footer";
-import Card from "./common/ssp-card";
-import Guide from "./common/ssp-guide";
 import SideImage from "./common/ssp-image";
 import OtpUi from "./common/ssp-otp";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { Box, Container, CssBaseline, Stack } from "@mui/material";
+import Grid from "@mui/material/Grid";
+import Image from "next/image";
 
 const theme = createTheme({
   typography: {
@@ -17,14 +16,37 @@ export default function Authenticate() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <Header />
-        <CssBaseline />
-        <Box>
-          <Stack direction="row" justifyContent="space-between">
-            <SideImage />
-            <OtpUi />
-          </Stack>
-        </Box>
+        <Grid container direction="column"></Grid>
+        <Grid item>
+          <Header />
+          <CssBaseline />
+        </Grid>
+        <Grid item sx={{ height: "100%", position: "fixed", width: "100%" }}>
+          {" "}
+          <Box sx={{ height: "100%" }}>
+            <Grid container sx={{ height: "100%" }}>
+              <Grid
+                item
+                xs={8}
+                sx={{
+                  height: "100%",
+                  display: { xs: "none", sm: "none", md: "block" },
+                }}
+              >
+                <div className={"image-container"}>
+                  <Image
+                    src="/home_logo.png"
+                    layout="fill"
+                    className={"image"}
+                  />
+                </div>
+              </Grid>
+              <Grid item xs={12} sm={12} xm={4} md={4}>
+                <OtpUi />
+              </Grid>
+            </Grid>
+          </Box>
+        </Grid>
       </ThemeProvider>
     </>
   );
--- src/components/common/ssp-header.jsx
+++ src/components/common/ssp-header.jsx
@@ -2,15 +2,18 @@ import { AppBar, Toolbar } from "@mui/material";
 import styles from "./Header.module.css";
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "./theme";
+import { Box } from "@mui/material";
 
 export default function Header() {
   return (
     <ThemeProvider theme={theme}>
-      <AppBar position="static">
+      <Box>
+      <AppBar position="sticky">
         <Toolbar>
           <img src="/ssp-logo.svg" alt="SIXT" className={styles.responsive} />
         </Toolbar>
       </AppBar>
+      </Box>
     </ThemeProvider>
   );
 }
